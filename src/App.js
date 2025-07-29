f172f9ba31cbc9e12e2b369585105abc2b80d98f
SushmaKumari777 <sushmapriya271005@gmail.com>
2025-07-29 08:01:37 +0530

    hgu

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
         <Route path="/allm" element={<Allm />} />
         <Route path="/jsr" element={<Jsr />} />
         <Route path="/strapkurti" element={<Strapkurti />} />
-        <Route path="/Addtocart" element={<Addtocart />} />
+        <Route path="/addtocart" element={<Addtocart />} />
 
 
 
