9993adfb27e844f2f1c42b48cf5492f08dfb19df
SushmaKumari777 <sushmapriya271005@gmail.com>
2025-08-12 18:55:01 +0530

    change

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -28,12 +28,55 @@ const Checkout = () => {
                                         </Row>
                                     </Form>
                                     <br></br>
-                                    <Form.Control placeholder="address" />
-                                    <Form.Control placeholder="address2" />                                   
+                                    <Form.Group className="mb-3" controlId="formGridAddress1">
+                                        <Form.Label>Address</Form.Label>
+                                        <Form.Control placeholder="1234 Main St" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="formGridAddress2">
+                                        <Form.Label>Address 2</Form.Label>
+                                        <Form.Control placeholder="Apartment, studio, or floor" />
+                                    </Form.Group>
+
+                                    <Row className="mb-3">
+                                        <Form.Group as={Col} controlId="formGridCity">
+                                            <Form.Label>City</Form.Label>
+                                            <Form.Control />
+                                        </Form.Group>
+
+                                        <Form.Group as={Col} controlId="formGridState">
+                                            <Form.Label>State</Form.Label>
+                                            <Form.Select defaultValue="Choose...">
+                                                <option>Choose...</option>
+                                                <option>...</option>
+                                            </Form.Select>
+                                        </Form.Group>
+
+                                        <Form.Group as={Col} controlId="formGridZip">
+                                            <Form.Label>Zip</Form.Label>
+                                            <Form.Control />
+                                            <br></br>
+                                        </Form.Group>
+                                        
+                                        <Form.Group className="mb-3" controlId="formGroupEmail">
+                                            <Form.Control type="phone" placeholder="Phone" />
+                                        </Form.Group>
+                                    </Row>
+                                    <Form.Group className="mb-3" id="formGridCheckbox">
+                                        <Form.Check type="checkbox" label="Text me with news and offers" />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                <h3>Payment</h3>
+                                <p>All transactions are secure and encrypted.</p>
                                 </Col>
                             </Row>
                         </Col>
-                        <Col></Col>
+                        <Col>
+                        
+                        </Col>
                     </Row>
                 </Container>
             </section>
